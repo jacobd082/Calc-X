b596672e47bc106f3db01e3e37dde4ec2d64ef51
Jacob <89097881+jacobd082@users.noreply.github.com>
2023-05-25 22:02:32 -0500

    Update

--- math.js
+++ math.js
@@ -13,6 +13,9 @@ function formatNumber(number) {
 
 let ans = "0"
 
+function deleteResult(element) {
+    element.remove();
+}  
 
 function solve(expr) {
     expr = expr.toLowerCase()
@@ -47,10 +50,10 @@ function solve(expr) {
     
     function done(re, isError) {
     if (isError) {
-        document.getElementById("results").innerHTML='<p style="margin:0;color:orange;">'+expr+'</p><p style="font-size: 20px;margin:0;">'+re+'</p><hr style="border-color:gray;">' + document.getElementById("results").innerHTML
+        document.getElementById("results").innerHTML='<div class="result-div"><button class="delete-button" onclick="deleteResult(this.parentNode)"><img src="icons/delete.png" width="10px" alt="Delete"></button><p style="margin: 0;color: orange;">'+expr+'</p><p style="font-size: 25px; margin: 0;">'+re+'</p><hr style="border-color: gray;"></div>' + document.getElementById("results").innerHTML
         return
     }
-    document.getElementById("results").innerHTML='<p style="margin:0;">'+expr+'</p><p style="font-size: 25px;margin:0;">'+re+'</p><hr style="border-color:gray;">' + document.getElementById("results").innerHTML
+    document.getElementById("results").innerHTML='<div class="result-div"><button class="delete-button" onclick="deleteResult(this.parentNode)"><img src="icons/delete.png" width="10px" alt="Delete"></button><p style="margin: 0;color: #d9d9d9;">'+expr+'</p><p style="font-size: 25px; margin: 0;">'+re+'</p><hr style="border-color: gray;"></div>' + document.getElementById("results").innerHTML
     ans = re
     }
 }
